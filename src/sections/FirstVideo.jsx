c2d32aff59532d095b3d0ae273e296128eaae398
Abdullahnabil22 <abdullahn.work@gmail.com>
2025-11-04 01:00:54 +0200

    fix: correct class name in GSAP setup and fix typo in onloadedmetadata

--- src/sections/FirstVideo.jsx
+++ src/sections/FirstVideo.jsx
@@ -6,7 +6,7 @@ function FirstVideo() {
   const videoRef = useRef(null);
 
   useGSAP(() => {
-    gsap.set(".first-vd", { marginTop: "-150vh", opacity: 0 });
+    gsap.set(".first-vd-wrapper", { marginTop: "-150vh", opacity: 0 });
 
     const tl = gsap.timeline({
       scrollTrigger: {
@@ -18,12 +18,14 @@ function FirstVideo() {
       },
     });
 
-    tl.to(".hero-section", { opacity: 0, delay: 0.5, ease: "power1.inOut" }).to(
-      ".first-vd-wrapper",
-      { opacity: 1, duration: 2, ease: "power1.inOut" }
-    );
+    tl.to(".hero-section", { delay: 0.5, opacity: 0, ease: "power1.inOut" });
+    tl.to(".first-vd-wrapper", {
+      opacity: 1,
+      duration: 2,
+      ease: "power1.inOut",
+    });
 
-    videoRef.current.onloadedmetadate = () => {
+    videoRef.current.onloadedmetadata = () => {
       tl.to(
         videoRef.current,
         {
